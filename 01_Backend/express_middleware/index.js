ba17586f845bfca3f1e088a5fa849139d99fb62b
bobby0015 <contact.dcdeveloper@gmail.com>
2026-03-15 23:47:07 +0530

    Created the route health-checkup which takes two inputs username and password and one param 1or2 as an kidney ID. And create a small logic to check how to routes work with express.

--- 01_Backend/express_middleware/index.js
+++ 01_Backend/express_middleware/index.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express()
 const PORT = 3000;
 
+app.get("/helath-checkup",(req,res)=>{
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidney = req.query.kidneyID;
+
+    if(username != "Divyam" || password != "pass"){
+        res.status(400).json({
+            msg : "Something up with your inputs"
+        })
+    }
+
+    if(kidney != 1 && kidney !=2) {
+        res.status(400).json({
+            msg : "Something up with your inputs"
+        })
+    }
+
+    res.json({
+        msg : "Your kidney is fine!"
+    }) 
+})
+
 app.listen(PORT,()=>{
     console.log("My server is running on PORT : " + PORT)
 })

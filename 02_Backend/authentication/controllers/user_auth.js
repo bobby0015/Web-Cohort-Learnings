6005e5d45e0daa4e7cafc764720096fef6373a3d
bobby0015 <contact.dcdeveloper@gmail.com>
2026-04-10 18:48:34 +0530

    evrything from backend

--- 02_Backend/authentication/controllers/user_auth.js
+++ 02_Backend/authentication/controllers/user_auth.js
@@ -1,7 +1,7 @@
+const user = require("../models/user")
+
 const singup = (req,res) => {
     const {username,email,password} = req.body;
-
-    res.send({username,email,password})
 }
 
 module.exports = {singup}

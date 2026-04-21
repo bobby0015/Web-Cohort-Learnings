892d19c457fadc528b99ff18b04916846a0f0920
bobby0015 <contact.dcdeveloper@gmail.com>
2026-04-22 00:33:48 +0530

    Create a new customButton component which can be used multiple times at various location 🙂

--- 02_Frontend/Counter_App/src/App.jsx
+++ 02_Frontend/Counter_App/src/App.jsx
@@ -4,15 +4,23 @@ const App = () => {
 
   const [count, setCount] = useState(0)
 
-  function updateCount() {
-    setCount(count + 1)
-  }
 
   return (
     <div>
-      <button onClick={updateCount}>Count {count}</button>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
+}
+
+function CustomButton (props) {
+
+  function updateCount() {
+    props.setCount(props.count + 1)
+  }
+
+  return <button onClick={updateCount}>
+    Counter {props.count}
+  </button>
 }
 
 export default App

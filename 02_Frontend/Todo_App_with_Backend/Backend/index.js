4499e81b2bad9e1203bf8ec41074d33b18e3299c
bobby0015 <contact.dcdeveloper@gmail.com>
2026-04-23 02:08:53 +0530

    added the logic of validating the payload

--- 02_Frontend/Todo_App_with_Backend/Backend/index.js
+++ 02_Frontend/Todo_App_with_Backend/Backend/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express()
 
 //Global middlewares
 app.use(express.json())
 
 app.post("/todo", function(req,res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload)
 
+    if(!parsePayload.success){
+        return res.status(411).json({
+            msg : 'Please enter the valid detials'
+        })
+    }
+
+    // Put the data in mongoDB
 })
 
 app.get("/todos", function(req,res){
@@ -13,7 +23,16 @@ app.get("/todos", function(req,res){
 })
 
 app.put("/completed", function(req,res){
-    
+    const updatePayload = req.body
+    const parsePayload = updateTodo.safeParse(updatePayload)
+
+    if(!parsePayload.success){
+        return res.status(411).json({
+            msg : "Inputs are not correct"
+        })
+    }
+
+    // Put the status in mongoDB
 })
 
 const PORT = 3000

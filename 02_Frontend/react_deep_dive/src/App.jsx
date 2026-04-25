cc15b3bc0dbabc8fabf48da2f4d4a1798fdc35ca
bobby0015 <contact.dcdeveloper@gmail.com>
2026-04-26 01:26:10 +0530

    The example of how the re rending works.

--- 02_Frontend/react_deep_dive/src/App.jsx
+++ 02_Frontend/react_deep_dive/src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
 const App = () => {
+
+  const [value,setValue] = useState('Divyam')
+
+  const randomValues = () => {
+    setValue(Math.random)
+  }
+
   return (
     <React.Fragment>
-      <Header title="My name is Divyam" />
+      <button onClick={randomValues}>Click me to change the name</button>
+      <Header title={`My name is ${value}`} />
       <Header title="My name is Chauhan" />
     </React.Fragment>
   )

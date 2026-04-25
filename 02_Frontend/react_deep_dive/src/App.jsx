d785f846375dad7e3620f2c9ae89d9f6776d4310
bobby0015 <contact.dcdeveloper@gmail.com>
2026-04-25 15:17:21 +0530

    We cannot reneder two components without a parednt div or a parent component. Therefore, here is the example how can we do it.

--- 02_Frontend/react_deep_dive/src/App.jsx
+++ 02_Frontend/react_deep_dive/src/App.jsx
@@ -1,9 +1,16 @@
 const App = () => {
   return (
     <div>
-      My app
+      <Header title="My name is Divyam" />
+      <Header title="My name is Chauhan" />
     </div>
   )
+}
+
+const Header = (props) => {
+  return <div>
+    {props.title}
+  </div>
 }
 
 export default App

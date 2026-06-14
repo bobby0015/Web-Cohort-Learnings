a4523fad012447d5de1fba62fe0fe0aa8f847199
bobby0015 <contact.dcdeveloper@gmail.com>
2026-06-14 23:29:16 +0530

    Created an Todo Componet which takes todo as an input and define the interface for that

--- 02_Typescript/src/App.tsx
+++ 02_Typescript/src/App.tsx
@@ -2,10 +2,24 @@
 
 const App = () => {
   return (
-    <div className="text-6xl">
-      Hello world
-    </div>
+    <>
+      <Todo title='My todo' desc='This is my todo' success={false} />
+    </>
   )
 }
 
 export default App
+
+interface TodoProp {
+  title: string,
+  desc: string,
+  success: boolean
+}
+
+const Todo = (prop: TodoProp) => {
+  return <div>
+    <h1>{prop.title}</h1>
+    <h1>{prop.desc}</h1>
+    <h1>{prop.success === true ? 'Completed' : "Pending"}</h1>
+  </div>
+}
